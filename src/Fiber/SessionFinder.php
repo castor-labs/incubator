4f4a703bb1c68c31cae9d664b0dd2bc6f08b404a
Matias Navarro Carter <mnavarrocarter@gmail.com>
2021-05-21 20:07:29 +0100

    Add the session finder

---
+++ src/Fiber/SessionFinder.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @project Castor Incubator
+ * @link https://github.com/castor-labs/incubator
+ * @package castor/incubator
+ * @author Matias Navarro-Carter mnavarrocarter@gmail.com
+ * @license MIT
+ * @copyright 2021 CastorLabs Ltd
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Castor\Fiber;
+
+use Castor\Net\Http;
+use RuntimeException;
+
+/**
+ * Trait SessionFinder.
+ */
+trait SessionFinder
+{
+    private function findSession(Http\Context $context): Session
+    {
+        foreach ($context->all() as $value) {
+            if ($value instanceof Session) {
+                return $value;
+            }
+        }
+
+        throw new RuntimeException('You must include Castor\Fiber\SessionSupport middleware in your pipeline');
+    }
+}
